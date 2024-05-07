cc0e313a5d2363fbed8ef0986123634f26bde658
minhquantb00 <minhquantb00@gmail.com>
2024-05-07 13:54:47 +0700

    Start header

--- ShopClothes.ClientApp/my-app/src/views/homeItem/HomeHeader.vue
+++ ShopClothes.ClientApp/my-app/src/views/homeItem/HomeHeader.vue
@@ -1,18 +1,23 @@
 <template>
-  <v-card style="border-radius: 0;" color="grey-lighten-3" max-width="100%">
-    <v-layout >
+  <v-card style="border-radius: 0" color="grey-lighten-3" max-width="100%">
+    <v-layout>
       <v-app-bar class="set-app-bar" color="white">
         <template v-slot:prepend>
           <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </template>
 
         <div>
-          <img src="../../assets/image-removebg-preview (1).png" style="width: 180px;"/>
+          <router-link to="/">
+            <img
+            src="../../assets/image-removebg-preview (1).png"
+            style="width: 180px"
+          />
+          </router-link>
         </div>
 
         <v-spacer></v-spacer>
-        <div class="d-flex" style="margin-right: 50px;">
-          <v-btn text class="menu-item" style="padding-left: 0;">
+        <div class="d-flex" style="margin-right: 50px">
+          <v-btn text class="menu-item" style="padding-left: 0">
             <v-icon left class="item">mdi-home</v-icon>
             Mới Về
           </v-btn>
@@ -30,34 +35,101 @@
           </v-btn>
         </div>
         <div style="margin-right: 100px; display: flex">
-          <div class="search d-flex" style="font-size: 16px;">
-            <v-btn  >
-              <v-icon style="font-size: 16px;">mdi-magnify</v-icon>
-
+          <div class="search d-flex" style="font-size: 16px">
+            <v-btn>
+              <v-icon style="font-size: 16px">mdi-magnify</v-icon>
             </v-btn>
             <input
-                class="search-input-item"
-                type="text"
-                placeholder="Tìm kiếm từ khóa"
-                style="font-size: 16px;"
-              />
+              class="search-input-item"
+              type="text"
+              placeholder="Tìm kiếm từ khóa"
+              style="font-size: 16px"
+            />
           </div>
 
-          <div v-if="userInfo" style="width: 100%; margin-right: 400px;" >
-            <v-btn style="margin-right: 20px; font-size: 16px;">
+          <div v-if="userInfo" style="width: 100%; margin-right: 400px">
+            <v-btn style="margin-right: 20px; font-size: 16px">
               <v-icon>mdi-heart</v-icon>
-              <p style="margin-left: 10px; text-transform:capitalize;">Giỏ hàng</p>
+              <p style="margin-left: 10px; text-transform: capitalize">
+                Giỏ hàng
+              </p>
             </v-btn>
 
             <v-btn icon>
-              <!-- <v-icon>mdi-dots-vertical</v-icon> -->
-              <div style="width: 40px; height: 40px; border-radius: 50%;">
-                <!-- <img style="width: 100%; height: 100%; border-radius: 50%;" src="https://miro.medium.com/v2/resize:fit:1400/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"/> -->
-                <v-avatar size="40" image="https://miro.medium.com/v2/resize:fit:1400/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"/>
-              </div>
+              <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                location="bottom"
+              >
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                    color="black"
+                    variant="text"
+                    icon
+                    class="text-none"
+                    v-bind="props"
+                  >
+                    <v-avatar
+                      size="50"
+                      image="https://miro.medium.com/v2/resize:fit:1400/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"
+                    ></v-avatar>
+                  </v-btn>
+                </template>
+
+                <v-card min-width="300" style="margin-top: 8px">
+                  <v-list>
+                    <v-list-item
+                      class="user-list-item"
+                      prepend-avatar="https://miro.medium.com/v2/resize:fit:1400/1*rKl56ixsC55cMAsO2aQhGQ@2x.jpeg"
+                      :subtitle="userInfo.Email"
+                      :title="userInfo.FullName"
+                    >
+                    </v-list-item>
+                  </v-list>
+                  <v-divider></v-divider>
+
+                  <v-list>
+                    <v-list-item>
+                      <v-title> Giỏ hàng của tôi </v-title>
+                      <template v-slot:append>
+                        <v-badge color="purple" content="6" inline></v-badge>
+                      </template>
+                    </v-list-item>
+                  </v-list>
+                  <v-divider></v-divider>
+                  <v-list-item>
+                    <v-title> Thông báo </v-title>
+                    <template v-slot:append>
+                      <v-badge color="purple" content="6" inline></v-badge>
+                    </template>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-title> Tin nhắn </v-title>
+                    <template v-slot:append>
+                      <v-badge color="purple" content="6" inline></v-badge>
+                    </template>
+                  </v-list-item>
+
+                  <v-divider></v-divider>
+                  <a href="/">
+                    <v-btn
+                      :disabled="loading"
+                      :loading="loading"
+                      size="x-large"
+                      style="width: 100%"
+                      @click="logout()"
+                      ><v-icon> mdi-logout </v-icon>Đăng xuất</v-btn
+                    >
+                  </a>
+                </v-card>
+              </v-menu>
             </v-btn>
           </div>
-          <div v-else class="d-flex align-items-center mt-2 ml-7">
+          <div
+            v-else
+            class="d-flex mt-2 ml-7"
+            style="align-items: center; margin-right: 400px"
+          >
             <router-link to="/login" class="account-link">
               <v-btn variant="outlined" class="ma-2"> Đăng nhập </v-btn>
             </router-link>
@@ -81,13 +153,21 @@ export default {
       userInfo: localStorage.getItem("userInfo")
         ? JSON.parse(localStorage.getItem("userInfo"))
         : null,
+      menu: false,
     };
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      localStorage.removeItem("userInfo");
+    },
   },
 };
 </script>
 
 <style lang="css" scoped>
-.set-app-bar{
+.set-app-bar {
   display: flex;
   justify-content: space-evenly;
   padding: 0 400px;
@@ -97,20 +177,172 @@ export default {
 }
 .search-input-item {
   border: none;
-
 }
 
 .search-input-item:focus {
   outline: none;
 }
-.menu-item{
+.menu-item {
   padding: 0 30px;
   font-size: 16px;
 }
-.menu-item .item{
+.menu-item .item {
   margin-right: 14px;
+}
+.user-header-link {
+  text-decoration: none;
+}
+.user-list-item:hover {
+  color: blueviolet;
 }
 .account-link {
   color: black;
+}
+#header-course {
+  height: 72px;
+  /* background-color: rgb(255, 255, 255); */
+  background: transparent;
+  backdrop-filter: blur(40px);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  padding: 0 40px;
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+}
+.logo-header {
+  line-height: 72px;
+  background-color: rgb(226, 226, 226);
+  border: none;
+}
+.search-header {
+  padding-left: 20px;
+
+  border: 1px rgb(61, 60, 60) solid;
+  margin: 0 40px;
+  border-radius: 40px;
+  line-height: 45px;
+}
+.search-header input {
+  width: 420px;
+  display: block;
+}
+.search-input-item {
+  border: none;
+
+  margin-left: 20px;
+}
+.search-input-item:focus {
+  outline: none;
+}
+.header-left .my-course:hover {
+  cursor: pointer;
+}
+.header-left .my-infor:hover {
+  cursor: pointer;
+}
+.my-infor {
+  margin-left: 40px;
+}
+.dropdownmenu li {
+  display: block;
+}
+.dropdownmenu li a {
+  text-decoration: none;
+  color: black;
+}
+#submenu {
+  /* left: 0; */
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  visibility: hidden;
+  z-index: 1;
+}
+.dropdownmenu li:hover ul#submenu {
+  opacity: 1;
+  top: 40px;
+  visibility: visible;
+}
+.type-menu {
+  position: absolute;
+}
+.hover-course-me {
+  padding: 10px;
+  border: 1px solid rgb(196, 196, 196);
+  margin-top: 24px;
+  width: 300px;
+  border-radius: 3px;
+  display: none;
+  color: black;
+  z-index: 6;
+  background: white;
+  box-shadow: 1px 1px 4px 1px rgb(205, 205, 205);
+}
+.hover-teaching {
+  padding: 10px;
+  border: 1px solid rgb(196, 196, 196);
+  margin-top: 24px;
+  width: 300px;
+  border-radius: 3px;
+  color: black;
+  display: none;
+  z-index: 6;
+  background: white;
+  box-shadow: 1px 1px 4px 1px rgb(205, 205, 205);
+}
+.teaching-header:hover .hover-teaching {
+  display: block;
+}
+.teaching-header:hover {
+  color: blueviolet;
+}
+.teaching-header {
+  position: absolute;
+}
+.course-me:hover {
+  color: blueviolet;
+}
+.course-me:hover .hover-course-me {
+  display: block;
+}
+.course-me {
+  position: absolute;
+}
+.cart-header {
+  position: absolute;
+}
+.hover-cart {
+  padding: 10px;
+  border: 1px solid rgb(196, 196, 196);
+  margin-top: 12px;
+  width: 300px;
+  border-radius: 3px;
+  left: 0;
+  color: black;
+  display: none;
+  z-index: 6;
+  background: white;
+  box-shadow: 1px 1px 4px 1px rgb(205, 205, 205);
+}
+.cart-header:hover {
+  color: blueviolet;
+}
+.cart-header:hover .hover-cart {
+  display: block;
+}
+.text-type-link {
+  text-decoration: none;
+  color: black;
+}
+.text-type-link:hover {
+  color: blueviolet;
+}
+.header-content-user-link {
+  text-decoration: none;
+  color: black;
+}
+.header-content-user-link:hover {
+  color: blueviolet;
 }
 </style>
